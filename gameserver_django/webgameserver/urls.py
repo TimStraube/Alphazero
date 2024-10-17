9901980a57a5c9c0f8061b20a8fbe2fb04618c4a
TimStraube <tileone02@posteo.de>
2024-10-17 17:34:11 +0200

    Small file integrations

--- gameserver_django/webgameserver/urls.py
+++ gameserver_django/webgameserver/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import menu, game
+from .views import menu, game, won, loss
 
 urlpatterns = [
     path('', menu, name='menu'),
     path('game/', game, name='game'),
+    path('won/', won, name='won'),
+    path('loss/', loss, name='loss'),
 ]
 
--- gameserver_django/webgameserver/views.py
+++ gameserver_django/webgameserver/views.py
@@ -6,5 +6,11 @@ def menu(request):
 def game(request):
     return render(request, 'game.html', {}, status=200)
 
+def won(request):
+    return render(request, 'won.html', {}, status=200)
+
+def loss(request):
+    return render(request, 'loss.html', {}, status=200)
+
 def custom_404(request, exception):
     return render(request, '404.html', {}, status=404)
